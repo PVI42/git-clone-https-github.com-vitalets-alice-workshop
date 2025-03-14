940b5858bf6bdad36d51ba618bbb90ffb6f1328a
PVI42 <perkovskiy@list.ru>
2025-03-14 14:01:58 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-src/index.js
+const micro = require('micro');
+
+const server = micro(async (req, res) => {
+  return 'Running';
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
